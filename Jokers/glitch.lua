bfd2f15dca155a79a33231a6d849305ab806031e
StarUndrscre <53362212+StarUndrscre@users.noreply.github.com>
2024-11-20 23:30:38 -0800

    finish functionallity for the first joker!! image is a placeholder; ryoko said they'd be making a new texture specifically for this pack

--- Jokers/glitch.lua
+++ Jokers/glitch.lua
@@ -1,30 +1,51 @@
-SMODS.Joker{
+SMODS.Atlas{
+  key = "glitch",
+  px = 69,
+  py = 93,
+  path = "glitch.png"
+}
+
+local joker = {
   key = "glitch",
   loc_txt = {
     name = "Glitch",
     text = {
-      "Copies ability of a random {C:joker}Joker {} in hand"
+      "Copies ability of a",
+      "random {C:attention}Joker{} in hand"
     }
   },
+  atlas = "glitch",
   rarity = 3,
   cost = 8,
+  blueprint_compat = true,
+  discovered = true,
   calculate = function(self, card, context)
     if context.joker_main then
-      local other_joker = nil
-      for i = 1, #G.jokers.cards do
-        if G.jokers.cards[i] == self then other_joker = G.jokers.cards[i+1] end
+      local other_joker = pseudorandom_element(G.jokers.cards, pseudoseed('glitch'))
+
+      if other_joker == self then
+        other_joker = pseudorandom_element(G.jokers.cards, pseudoseed('glitch_not_self_please'))
       end
-      if other_joker and other_joker ~= self then
-        context.blueprint = (context.blueprint and (context.blueprint + 1)) or 1
-        context.blueprint_card = context.blueprint_card or self
-        if context.blueprint > #G.jokers.cards + 1 then return end
-        local other_joker_ret = other_joker:calculate_joker(context)
-        if other_joker_ret then 
-          other_joker_ret.card = context.blueprint_card or self
-          other_joker_ret.colour = G.C.BLUE
-          return other_joker_ret
-        end
+
+      if other_joker.config.center.blueprint_compat == false then
+        other_joker = pseudorandom_element(G.jokers.cards, pseudoseed('glitch_compatible_please'))
+      end
+
+      if other_joker and other_joker.config.center.blueprint_compat then
+        context.blueprint_card = card
+        G.E_MANAGER:add_event(Event({
+          trigger = immediate,
+          func = function()
+            other_joker:juice_up(0.3, 0.3)
+            return true
+          end
+        }))
+        other_joker:calculate_joker(context)
       end
     end
   end
 }
+
+SMODS.Joker(joker)
+print(joker.key)
+return joker
